2fe1baeb1abc0c69b7307753cb763908f317f0a3
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2023-08-03 12:40:04 +0100

    setup code base

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <main className="h-screen flex">
+      <div className="pt-[185px]">
+        <h1 className="text-5xl	font-semibold	">Hi 👋🏿, I&apos;m Mayowa</h1>
+        <h2 className="text-2xl font-light mt-3">A software developer based in Lagos, Nigeria.</h2>
+      </div>
+    </main>
+  )
+}
